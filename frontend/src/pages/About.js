5110856d77bce5d840f85e9e795155f247a712d0
joeycalt <joeycalt@yahoo.com>
2022-11-09 11:35:00 -0500

    added about info to page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react";
+
 function About(props) {
-    return <h1>About</h1>;
-  }
   
-  export default About;
+  const [about, setAbout] = useState(null);
+
+  const getAboutData = async () => {
+    
+    const response = await fetch(props.URL + "about");
+    
+    const data = await response.json();
+    
+    setAbout(data);
+  };
+
+  useEffect(() => getAboutData(), []);
+
+  const loaded = () => (
+    <div>
+      <h2>{about.name}</h2>
+      <h3>{about.email}</h3>
+      <p>{about.bioOne}</p>
+      <br />
+      <p>{about.bioTwo}</p>
+      <br />
+      <p>{about.bioThree}</p>
+      <div>
+        <img src={about.imageOne} />
+      </div>
+      <div>
+        <img src={about.imageTwo} />
+      </div>
+    </div>
+  );
+
+  return about ? loaded() : <h1>Loading...</h1>;
+}
+
+export default About;
