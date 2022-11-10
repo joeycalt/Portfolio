4d912ce5da373e25a1577e0077998d4b7d9b8003
joeycalt <joeycalt@yahoo.com>
2022-11-10 11:54:37 -0500

    rendered images and buttons for each project on project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,41 @@
+import { useState, useEffect } from "react";
+
 function Projects(props) {
-    return <h1>Projects</h1>;
-  }
+  const [projects, setProjects] = useState(null);
+  useEffect(() => {
+
+  const getProjectsData = async () => {
+    
+    const response = await fetch(props.URL + "projects");
+    
+    const data = await response.json();
+    
+    setProjects(data);
+
+  };
   
-  export default Projects;
+  getProjectsData()}, [props.URL]);
+
+  const loaded = () => {
+    return projects?.map((project) => (
+      <div key={project.name}>
+        <h1 className="project">{project.name}</h1>
+        <img className="project-img" src={project.image} alt='' />
+       
+        <div>
+            <a href={project.git}>
+          <button className="gitbtn">Github</button>
+        </a>
+        <a href={project.live}>
+          <button className="herokubtn">Live Site</button>
+        </a>
+        </div>
+        
+      </div>
+    ));
+  };
+
+  return projects ? loaded() : <h1>Loading...</h1>;
+}
+
+export default Projects;
