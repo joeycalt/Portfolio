f772a2a7978ec85614961bab857c3eed11af568a
joeycalt <joeycalt@yahoo.com>
2022-11-11 13:50:08 -0500

    added typewriter effect to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 function Main(props) {
     return (
       <>
+      <div className="wrapper-title">
         <h1 className="main-title">Frontend Developer by day... and night</h1>
+        </div>
         <br />
-        <h4>Check out what I've made!</h4>
+        <h4 className="second-title">Check out what I've made!</h4>
       </>
     )
   }
