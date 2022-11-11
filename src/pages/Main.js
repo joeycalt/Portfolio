e09baba428c56bdb7ab4e8d1ad702e8cca668bb2
joeycalt <joeycalt@yahoo.com>
2022-11-11 13:15:42 -0500

    started styling home page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,11 @@
 function Main(props) {
-    return <h1>Main</h1>;
+    return (
+      <>
+        <h1 className="main-title">Frontend Developer by day... and night</h1>
+        <br />
+        <h4>Check out what I've made!</h4>
+      </>
+    )
   }
   
   export default Main;
