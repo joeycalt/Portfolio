18ab65292625d0b0c8150f1ac8cd16039ac61856
joeycalt <joeycalt@yahoo.com>
2022-11-11 13:01:14 -0500

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function Header(props) {
-    return <h1>Header</h1>;
+    return (
+      <>
+        <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">JOEY_CALT</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </>
+    )
   }
   
   export default Header;
