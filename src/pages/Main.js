2db18f3f3efc7e9793fd81dbd5e2f6a7885e4d4b
joeycalt <joeycalt@yahoo.com>
2022-11-17 15:25:37 -0500

    added image of myself to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,6 +12,8 @@ function Main(props) {
         <h4 className="second-title">Check out what I've made!</h4>
         <a href='/projects'>
         <Button className= 'main-button' variant= "outline-success">Check out my work!</Button>
+        <br />
+        <img className='my-image' src='https://i.im.ge/2022/11/18/SSm52D.5-7-22-Kristin-Danny-Chateau-Briand-Photos.jpg' alt=''></img>
         </a>
       </>
     )
