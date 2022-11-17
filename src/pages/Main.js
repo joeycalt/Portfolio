7a986fc7c99aeae3fa790acb2a78ceb58f3bb347
joeycalt <joeycalt@yahoo.com>
2022-11-17 10:14:07 -0500

    styled backgrounds and fonts

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,10 @@
 function Main(props) {
     return (
       <>
+        <h5 className="greeting">Hi, my name is</h5> <br />
+        <h1 className="name">Joey Caltabellotta</h1> <br />
       <div className="wrapper-title">
-        <h1 className="main-title">Frontend Developer by day... and night</h1>
+        <h1 className="main-title">I'm a Frontend Developer by day... and night</h1>
         </div>
         <br />
         <h4 className="second-title">Check out what I've made!</h4>
