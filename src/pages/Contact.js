93aa89e504226c9892f5a67242d662fcea082b81
joeycalt <joeycalt@yahoo.com>
2022-11-19 11:27:21 -0500

    fixed more errors again

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,11 +7,14 @@ function Contact(props) {
         <h1 className='contact-title-top'>Let's connect and build cool stuff!</h1>
         <h5 className='contact-title-bottom'>I am currently available to work</h5>
         <div className='info'>
-        <GeoAltFill className='contact' /> Great River, NY
+        <span className='contact-words' >
+        <GeoAltFill className='contact' /> Great River, NY</span>
         <br />
-        <PhoneFill className='contact' /> <a className='intouch' href='tel:631-889-4226'>631-889-4226</a>
+        <a className='intouch contact-words' href='tel:631-889-4226'>
+        <PhoneFill className='contact' /> 631-889-4226</a>
         <br />
-        <EnvelopeCheckFill className='contact' /> <a className='intouch' href= 'mailto: caltabellottajoey@gmail.com'>caltabellottajoey@gmail.com</a>
+        <a className='intouch contact-words' href= 'mailto: caltabellottajoey@gmail.com'>
+        <EnvelopeCheckFill className='contact' /> caltabellottajoey@gmail.com</a>
         </div>
       </>
     )
