c07a38cf0bb12b6e43afc7f68fed2c6b2f0e6b69
joeycalt <joeycalt@yahoo.com>
2022-11-21 10:19:37 -0500

    fixed an error

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,9 +12,9 @@ function Main(props) {
         <h4 className="second-title">Check out what I've made!</h4>
         <a href='/projects'>
         <Button className= 'main-button' variant= "outline-success">Check out my work!</Button>
+        </a>
         <br />
         <img className='my-image' src='https://i.im.ge/2022/11/18/SSm52D.5-7-22-Kristin-Danny-Chateau-Briand-Photos.jpg' alt=''></img>
-        </a>
       </>
     )
   }
