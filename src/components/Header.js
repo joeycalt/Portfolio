5842e36bf95aaabc31bed2d11dc53314c68cd226
joeycalt <joeycalt@yahoo.com>
2022-11-22 16:03:02 -0500

    added resume link to nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Github, Linkedin, House} from 'react-bootstrap-icons'
+import {Github, Linkedin, House} from 'react-bootstrap-icons';
+import Button from 'react-bootstrap/Button';
 
 function Header(props) {
     return (
@@ -17,6 +18,9 @@ function Header(props) {
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
+            <Nav.Link className='resume-button' href="https://pdfhost.io/v/NGTu3pHqN_JoeyCaltabellottaResume2022" target="_blank" rel="noreferrer" type="application/pdf" media="print">
+            <Button className= 'resume-button' variant= "outline-success">My resume</Button>
+              </Nav.Link>
           <Nav>
             <Nav.Link href="https://github.com/joeycalt" target="_blank" rel="noreferrer">
             <Github className='mygit' />
