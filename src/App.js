dbf17b7c7888ef419264b18ef96f8a424677cc59
joeycalt <joeycalt@yahoo.com>
2023-01-01 17:10:38 -0500

    deleted footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import About from './pages/About';
 import Contact from './pages/Contact';
