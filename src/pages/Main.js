f77539bc36976e77a1edf09f963a7e89e8630f57
joeycalt <joeycalt@yahoo.com>
2023-01-01 17:20:22 -0500

    edited main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,9 +5,8 @@ function Main(props) {
       <>
       <div className="wrapper-title">
         </div>
-        <p className='main-about'>I’m <strong className='joey'>Joey</strong> a <strong>frontend developer</strong> based in <strong>Long Island, NY.</strong> Previously a <strong>plumber</strong> (<em>new year, new me</em>). I specialize in <strong>React, Javascript, Node.js and more.</strong> </p>
+        <p className='main-about'>I’m <strong className='joey'>Joey,</strong> a <strong>frontend developer</strong> based in <strong>Long Island, NY.</strong> Previously a <strong>plumber</strong> (<em>new year, new me</em>). I specialize in <strong>React, Javascript, Node.js and more.</strong> </p>
         <br />
-        <h4 className="second-title">Check out what I've made!</h4>
         <a href='/projects'>
         <Button className= 'main-button' variant= "outline-success">My projects</Button>
         </a>
